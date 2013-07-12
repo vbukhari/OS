a75c0544b070975b3a9a8c08d5ec6e07f2533556
vbukhari <vbukhari86@gmail.com>
2013-07-12 12:52:43 -0400

    added some libraries

--- myshell.c
+++ myshell.c
@@ -5,3 +5,11 @@ Operating System Project - Summer 2013
 This file includes all the functions for myshell
 */
 
+#include <stdio.h>
+#include <dirent.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define MAX_BUFFER_SIZE 1024
+#define MAX_PATH_LENGHT 100
