a68ab29d255236fdb268575a8067a0eace8308c2
ksmendoz <ksmendoz@mail.usf.edu>
2013-07-12 15:05:12 -0400

    Update utility.c

--- utility.c
+++ utility.c
@@ -3,4 +3,29 @@ Vasim & Kenny
 Operating System Project - Summer 2013
 
 This file includes the main function of program
-*/
+*/#include"myshell.h"
+
+/*READ FILE LINES*/
+void File_Read( char * );
+
+/*EXECUTES COMMANDS*/
+void Exe_Cmnd();
+
+//CHANGE PATH THEN CLOSE
+void Close_IO();
+
+/*CHANGE IO DIRECTION*/
+void Change_IO( int  );/*permission to read*/
+
+/*ACTIVATES CHILD*/
+void Child_Activate();
+
+/*CONVERTS FILE CHARS TO TOKENS*/
+void TOKENIZE();
+
+/*PATH IS UPDATED*/
+void Path_NEW();
+
+/*DISPLAYS ERROR MESSAGE TO USER*/
+void ERROR( char * );
+
